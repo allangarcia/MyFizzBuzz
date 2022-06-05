3762a3851957c046118bee9ed98fdadf984feffc
Allan Garcia <allan.garcia@gmail.com>
2022-06-05 14:33:10 -0300

    Initial Commit

---
+++ Shared/MyFizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFizzBuzzApp.swift
+//  Shared
+//
+//  Created by Allan Garcia on 05/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MyFizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
